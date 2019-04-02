3f0b1dfc48785d2421a09d675a6ab9eb156dd49a
ar.papeta <ar.papeta@gmail.com>
2019-04-02 14:36:47 +0200

    Add logic for PLC

--- WheelWpf/MainWindow.xaml.cs
+++ WheelWpf/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.DirectX;
 using Microsoft.DirectX.DirectInput;
-using System.Windows.Interop;
-using System.Runtime.InteropServices;
 using System.Windows.Threading;
 using Sharp7;
 
@@ -22,16 +9,28 @@ namespace WheelWpf
 {
     public partial class MainWindow : Window
     {
+        int i = 0;
+        public short XAxis { get; set; }   //Right & Left
+        public short YAxis { get; set; }   //Front & back
+        public short ZAxis { get; set; }   //Velocity
+        public short RzAxis { get; set; }  //Reversal
+
+        public bool back { get; set; }      
+        public short velocityC { get; set; }  
+        public bool front { get; set; }
+        
+
         public int connectionResult;
+        
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        //MainWindow mw = new MainWindow();
         Device device;
-        S7Client client = new S7Client();                       //Check
+        S7Client client = new S7Client();                       
         DispatcherTimer timer = new DispatcherTimer();
-
+        
 
         private void timer_Tick(object sender, EventArgs e)
         {
@@ -40,9 +39,10 @@ namespace WheelWpf
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            
             timer.Tick += new EventHandler(timer_Tick);
             timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
-            timer.Start();         
+            timer.Start();
         }
 
         private void UpdateJoystickState()
@@ -51,64 +51,80 @@ namespace WheelWpf
             {
                 JoystickState j = device.CurrentJoystickState;
                     
-            string info = "";
-            byte[] buttons = j.GetButtons();
-            for (int i = 0; i < buttons.Length; i++)
-            {
-                if (buttons[i] != 0)
+                string info = "";
+                byte[] buttons = j.GetButtons();
+                for (int i = 0; i < buttons.Length; i++)
                 {
-                    info += "Button: " + i + " ";
+                    if (buttons[i] != 0)
+                    {
+                        info += "Button: " + i + " ";
+                    }
                 }
-            }
 
-            textBlock1.Text = j.ToString();
-            textBlock2.Text = info;
-
-            // Uses axises separate info         
-            
-            if (connectionResult == 0)
-            {
-                client.ConnectTo("192.168.49.2", 0, 1);
-                var writeBuffer = new byte[2];
-                short ydata = Convert.ToInt16(j.Y.ToString()); ;
-                S7.SetIntAt(writeBuffer, 0, ydata);           
-                client.DBWrite(15211, 0, writeBuffer.Length, writeBuffer);            
-                client.Disconnect();
-            }
+                textBlock1.Text = j.ToString();
+                textBlock2.Text = info;
+                
+                ProcessingJoyData(Convert.ToInt16(j.X), Convert.ToInt16(j.Y), Convert.ToInt16(j.Z), Convert.ToInt16(j.Rz));
+                if (connectionResult == 0)
+                {
+                    client.ConnectTo("192.168.49.2", 0, 1);
+                    ProcessingJoyData(Convert.ToInt16(j.X), Convert.ToInt16(j.Y),Convert.ToInt16(j.Z),Convert.ToInt16(j.Rz));
+                    var writeBuffer = new byte[2];
+                    //YAxis = Convert.ToInt16(j.Y);
+                    textBlock5.Text = YAxis.ToString();
+                    //short Y = Convert.ToInt16(j.Y.ToString());
+                    S7.SetIntAt(writeBuffer, 0, YAxis);           
+                    client.DBWrite(15211, 0, writeBuffer.Length, writeBuffer);
+                    //client.Disconnect();
+                }
             }
             catch (NullReferenceException)
             {              
                 label1.Foreground = Brushes.Red;
                 label1.FontWeight = FontWeights.UltraBold;
-                label1.Content = "Device not found! Please check the connection and try again.";                
+                label1.Content = "Device not found! Please check the connection and try again.";
+                //client.Disconnect();
                 //System.Diagnostics.Process.GetCurrentProcess().Kill();
                 //throw new NullReferenceException("Joystick not found! Please check the connection and try again.");
             }
         }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void CheckConnection()
         {
-            // Connection to the PLC
-            //var client = new S7Client();
+            // Check connection to the PLC
             connectionResult = client.ConnectTo("192.168.49.2", 0, 1);
             if (connectionResult == 0)
             {
                 textBlock4.FontWeight = FontWeights.UltraBold;
                 textBlock4.Foreground = Brushes.Green;
                 textBlock4.Text = "PLC CONNECTED";
- //               var writeBuffer = new byte[2];
- //               S7.SetIntAt(writeBuffer, 0, 15);
- //               client.DBWrite(15211, 0, writeBuffer.Length, writeBuffer);
             }
             else
             {
                 textBlock4.FontWeight = FontWeights.UltraBold;
                 textBlock4.Foreground = Brushes.Red;
-                textBlock4.Text = "PLC NOT FOUND";              
-            }    
+                textBlock4.Text = "PLC NOT FOUND";
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
             
-            //client.Disconnect();
-            //////////////////////////////////////////////////////////////////////////
+            //Check first connection
+            // Check connection to the PLC
+            connectionResult = client.ConnectTo("192.168.49.2", 0, 1);
+            if (connectionResult == 0)
+            {
+                textBlock4.FontWeight = FontWeights.UltraBold;
+                textBlock4.Foreground = Brushes.Green;
+                textBlock4.Text = "PLC CONNECTED";
+            }
+            else
+            {
+                textBlock4.FontWeight = FontWeights.UltraBold;
+                textBlock4.Foreground = Brushes.Red;
+                textBlock4.Text = "PLC NOT FOUND";
+            }
+            //CheckConnection();
             foreach (DeviceInstance instance in Manager.GetDevices(DeviceClass.GameControl, EnumDevicesFlags.AttachedOnly))
             {
                 device = new Device(instance.ProductGuid);
@@ -136,9 +152,28 @@ namespace WheelWpf
                 label1.Foreground = Brushes.Red;
                 label1.FontWeight = FontWeights.UltraBold; 
                 label1.Content = "Device NOT found";
-            }   
-            
-                
+            }                     
         }
+
+        void ProcessingJoyData(short LeftRight, short ForwardBack, short velocityCoef, short Reversal)
+        {
+            //Console.WriteLine("ss");           
+            YAxis = ForwardBack;
+            if (ForwardBack <= 40)
+            {
+                front = true;
+                back = false;
+                velocityC = Convert.ToInt16(((100 - velocityCoef) * (40 - ForwardBack))/40);               
+            }
+            if (ForwardBack < 60 && ForwardBack > 40) {
+                velocityC = 0;
+            }
+            if (ForwardBack > 60 )
+            {
+                velocityC = Convert.ToInt16(((100 - velocityCoef) * (ForwardBack-60))/40);
+            }
+            textBlock5.Text = velocityC.ToString();
+        }
+        
     }
 }
